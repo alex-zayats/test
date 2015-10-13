5e0a2e2126074dcd031da0a22c8c6dccb806392c
Alex <alex3953898@yandex.com>
2015-10-13 15:34:55 +0300

    add file

---
+++ mainController.js
@@ -0,0 +1,13 @@
+(function(app){
+ 
+    var mainCtrl = function( $scope, loadJSFactory ){
+ 
+        loadJSFactory.load('js/profileCtrl.js')
+            .then(function(){
+                console.log('script loaded');
+            });
+    };
+ 
+    app.controller('mainCtrl',['$scope', 'loadJSFactory', mainCtrl]);
+ 
+})(lazyloadapp);
